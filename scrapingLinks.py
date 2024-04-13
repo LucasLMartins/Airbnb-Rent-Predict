edef4ad69fb54da578d55036e42dbca73e053a95
LucasLMartins <lucasluh.morais@hotmail.com>
2024-04-13 16:08:45 -0300

    links scraping

---
+++ scrapingLinks.py
@@ -0,0 +1,93 @@
+import os
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import urlparse, parse_qs, urlencode
+
+def get_next_page_url(url, items_offset):
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    query_params['items_offset'] = items_offset
+    updated_query = urlencode(query_params, doseq=True)
+    return f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}?{updated_query}"
+
+def scrape_prices(url, num_pages):
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
+
+    all_prices = []
+    all_links = []
+
+    prev_page_source = None
+
+    for page in range(num_pages):
+        items_offset = page * 20
+        current_url = get_next_page_url(url, items_offset)
+
+        driver.get(current_url)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'pquyp1l')))
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'pquyp1l')))
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 't1jojoys')))
+        # WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="site-content"]/div/div[2]/div/div/div/div/div[1]/div[1]/div/div[2]/div/meta[3]')))
+
+        current_page_source = driver.page_source
+
+        if current_page_source == prev_page_source:
+            print("Reached end of results. Stopping scraping.")
+            break
+
+        prev_page_source = current_page_source
+
+        print(f"Scraping page {page + 1}")
+
+        # prices = driver.find_elements(By.CLASS_NAME, 'pquyp1l')
+        # titles = driver.find_elements(By.CLASS_NAME, 't1jojoys')
+        links = driver.find_elements(By.XPATH, '//*[@id="site-content"]/div/div[2]/div/div/div/div/div[1]/div')
+
+        print(len(links))
+
+        # print(f"Number of prices found on page {page + 1}: {len(prices)}")
+
+        # page_data = []
+        page_links = []
+        i = 1
+        for link in zip(links):
+            urlink = '//*[@id="site-content"]/div/div[2]/div/div/div/div/div[1]/div[{}]/div/div[2]/div/meta[3]'.format(i)
+            linkurl = driver.find_element(By.XPATH, urlink)
+            
+            link_value = linkurl.get_attribute("content") if link else None
+
+            page_links.append(link_value)
+            i = i + 1
+
+        # all_prices.extend(page_data)
+        all_links.extend(page_links)
+
+        time.sleep(1)
+
+    driver.quit()
+
+    return all_links
+
+def main():
+    url = 'https://www.airbnb.com.br/s/Paran%C3%A1--Brasil/homes'
+    num_pages = 5
+    links = scrape_prices(url, num_pages)
+
+    df = pd.DataFrame(links)
+    df.to_excel('airbnb_links.xlsx', index=False)
+    print("Prices exported to 'airbnb_links.xlsx'")
+
+    print("All Links:")
+    for link in links:
+        print(link)
+
+if __name__ == "__main__":
+    main()

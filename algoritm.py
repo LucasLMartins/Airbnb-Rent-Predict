14617f2c9286a6f4ebd11ac0909bf1de3e6d8f98
LucasLMartins <lucasluh.morais@hotmail.com>
2024-04-17 22:02:43 -0300

    user inputs

--- algoritm.py
+++ algoritm.py
@@ -20,70 +20,77 @@ X_train_encoded, X_test_encoded, y_train_encoded, y_test_encoded = train_test_sp
 model_encoded = LinearRegression()
 model_encoded.fit(X_train_encoded, y_train_encoded)
 
-# Fazer previsões com os dados codificados
-predictions_encoded = model_encoded.predict(X_test_encoded)
+# # Fazer previsões com os dados codificados
+# predictions_encoded = model_encoded.predict(X_test_encoded)
 
-# Calcular o erro quadrático médio
-mse_encoded = mean_squared_error(y_test_encoded, predictions_encoded)
-rmse = np.sqrt(mse_encoded)
-print('Mean Squared Error (Encoded):', mse_encoded)
-print('Root Mean Squared Error:', rmse)
+# # Calcular o erro quadrático médio
+# mse_encoded = mean_squared_error(y_test_encoded, predictions_encoded)
+# rmse = np.sqrt(mse_encoded)
+# print('Mean Squared Error (Encoded):', mse_encoded)
+# print('Root Mean Squared Error:', rmse)
+
+# titulo = input("Digite o título do imóvel: ")
+tipo = input("Digite o tipo do imóvel (Casa, Apartamento): ")
+# cidade = input("Digite a cidade: (Curitiba, Pinhais, São José dos Pinhais)")
+hospedes = input("Digite a quantidade de hóspedes: ")
+quartos = input("Digite a quantidade de quartos: ")
+camas = input("Digite a quantidade de camas: ")
+banheiros = input("Digite a quantidade de banheiros: ")
 
 # Utilizar o modelo treinado para fazer previsões com os dados pré-informados
 pre_informed_data = pd.DataFrame({
-    'titulo': [random.choice(['Apartmento acochegante', 'Casa tranquila', 'Condominio tranquilo']) for _ in range(3)],
-    'tipo': [random.choice(['Apartamento', 'Casa', 'Condominio']) for _ in range(3)],
-    'cidade': [random.choice(['Curitiba', 'Pinhais', 'São José dos Pinhais']) for _ in range(3)],
-    'estado': [random.choice(['Paraná']) for _ in range(3)],
-    'hospedes': [random.randint(1, 10) for _ in range(3)],
-    'quartos': [random.randint(1, 5) for _ in range(3)],
-    'camas': [random.randint(1, 10) for _ in range(3)],
-    'banheiros': [random.randint(1, 5) for _ in range(3)],
-    'Elevador': [random.randint(0, 1) for _ in range(3)],
-    'Cozinha': [random.randint(0, 1) for _ in range(3)],
-    'Microondas': [random.randint(0, 1) for _ in range(3)],
-    'Fogão': [random.randint(0, 1) for _ in range(3)],
-    'Mesadejantar': [random.randint(0, 1) for _ in range(3)],
-    'Wi-Fi': [random.randint(0, 1) for _ in range(3)],
-    'Ar-condicionado': [random.randint(0, 1) for _ in range(3)],
-    'TV': [random.randint(0, 1) for _ in range(3)],
-    'Secadordecabelo': [random.randint(0, 1) for _ in range(3)],
-    'Xampu': [random.randint(0, 1) for _ in range(3)],
-    'Condicionador': [random.randint(0, 1) for _ in range(3)],
-    'Saboneteparaocorpo': [random.randint(0, 1) for _ in range(3)],
-    'Águaquente': [random.randint(0, 1) for _ in range(3)],
-    'MáquinadeLavar': [random.randint(0, 1) for _ in range(3)],
-    'Secadora': [random.randint(0, 1) for _ in range(3)],
-    'Café': [random.randint(0, 1) for _ in range(3)],
-    'Fechadurainteligente': [random.randint(0, 1) for _ in range(3)],
-    'Refrigerador': [random.randint(0, 1) for _ in range(3)],
-    'Permitidoanimais': [random.randint(0, 1) for _ in range(3)],
-    'Permitidofumar': [random.randint(0, 1) for _ in range(3)],
-    'Banheira': [random.randint(0, 1) for _ in range(3)],
-    'Produtosdelimpeza': [random.randint(0, 1) for _ in range(3)],
-    'Básico': [random.randint(0, 1) for _ in range(3)],
-    'Cabides': [random.randint(0, 1) for _ in range(3)],
-    'Roupadecama': [random.randint(0, 1) for _ in range(3)],
-    'Cinema': [random.randint(0, 1) for _ in range(3)],
-    'Lareirainterna': [random.randint(0, 1) for _ in range(3)],
-    'Espaçodetrabalhoexclusivo': [random.randint(0, 1) for _ in range(3)],
-    'Itensbásicosdecozinha': [random.randint(0, 1) for _ in range(3)],
-    'Louçasetalheres': [random.randint(0, 1) for _ in range(3)],
-    'Frigobar': [random.randint(0, 1) for _ in range(3)],
-    'Lavalouças': [random.randint(0, 1) for _ in range(3)],
-    'Cafeteira': [random.randint(0, 1) for _ in range(3)],
-    'Torradeira': [random.randint(0, 1) for _ in range(3)],
-    'Entradaprivada': [random.randint(0, 1) for _ in range(3)],
-    'Áreadejantarexterna': [random.randint(0, 1) for _ in range(3)],
-    'Churrasqueira': [random.randint(0, 1) for _ in range(3)],
-    'Estacionamentoincluído': [random.randint(0, 1) for _ in range(3)],
-    'Jacuzziprivativa': [random.randint(0, 1) for _ in range(3)],
-    'Vistaparaojardim': [random.randint(0, 1) for _ in range(3)],
+    'titulo': ['Imóvel teste' for _ in range(1)],
+    'tipo': [tipo for _ in range(1)],
+    'cidade': ['Curitiba' for _ in range(1)],
+    'estado': ['Paraná' for _ in range(1)],
+    'hospedes': [int(hospedes) for _ in range(1)],
+    'quartos': [int(quartos) for _ in range(1)],
+    'camas': [int(camas) for _ in range(1)],
+    'banheiros': [int(banheiros) for _ in range(1)],
+    'Elevador': [random.randint(0, 1) for _ in range(1)],
+    'Cozinha': [random.randint(0, 1) for _ in range(1)],
+    'Microondas': [random.randint(0, 1) for _ in range(1)],
+    'Fogão': [random.randint(0, 1) for _ in range(1)],
+    'Mesadejantar': [random.randint(0, 1) for _ in range(1)],
+    'Wi-Fi': [random.randint(0, 1) for _ in range(1)],
+    'Ar-condicionado': [random.randint(0, 1) for _ in range(1)],
+    'TV': [random.randint(0, 1) for _ in range(1)],
+    'Secadordecabelo': [random.randint(0, 1) for _ in range(1)],
+    'Xampu': [random.randint(0, 1) for _ in range(1)],
+    'Condicionador': [random.randint(0, 1) for _ in range(1)],
+    'Saboneteparaocorpo': [random.randint(0, 1) for _ in range(1)],
+    'Águaquente': [random.randint(0, 1) for _ in range(1)],
+    'MáquinadeLavar': [random.randint(0, 1) for _ in range(1)],
+    'Secadora': [random.randint(0, 1) for _ in range(1)],
+    'Café': [random.randint(0, 1) for _ in range(1)],
+    'Fechadurainteligente': [random.randint(0, 1) for _ in range(1)],
+    'Refrigerador': [random.randint(0, 1) for _ in range(1)],
+    'Permitidoanimais': [random.randint(0, 1) for _ in range(1)],
+    'Permitidofumar': [random.randint(0, 1) for _ in range(1)],
+    'Banheira': [random.randint(0, 1) for _ in range(1)],
+    'Produtosdelimpeza': [random.randint(0, 1) for _ in range(1)],
+    'Básico': [random.randint(0, 1) for _ in range(1)],
+    'Cabides': [random.randint(0, 1) for _ in range(1)],
+    'Roupadecama': [random.randint(0, 1) for _ in range(1)],
+    'Cinema': [random.randint(0, 1) for _ in range(1)],
+    'Lareirainterna': [random.randint(0, 1) for _ in range(1)],
+    'Espaçodetrabalhoexclusivo': [random.randint(0, 1) for _ in range(1)],
+    'Itensbásicosdecozinha': [random.randint(0, 1) for _ in range(1)],
+    'Louçasetalheres': [random.randint(0, 1) for _ in range(1)],
+    'Frigobar': [random.randint(0, 1) for _ in range(1)],
+    'Lavalouças': [random.randint(0, 1) for _ in range(1)],
+    'Cafeteira': [random.randint(0, 1) for _ in range(1)],
+    'Torradeira': [random.randint(0, 1) for _ in range(1)],
+    'Entradaprivada': [random.randint(0, 1) for _ in range(1)],
+    'Áreadejantarexterna': [random.randint(0, 1) for _ in range(1)],
+    'Churrasqueira': [random.randint(0, 1) for _ in range(1)],
+    'Estacionamentoincluído': [random.randint(0, 1) for _ in range(1)],
+    'Jacuzziprivativa': [random.randint(0, 1) for _ in range(1)],
+    'Vistaparaojardim': [random.randint(0, 1) for _ in range(1)],
 })
 print("Pre-informed data:")
 for data in pre_informed_data.values:
     print(data)
-
 
 # Align the columns of pre_informed_data with the training data
 pre_informed_data_aligned = pre_informed_data.reindex(columns=X_train_encoded.columns, fill_value=0)
